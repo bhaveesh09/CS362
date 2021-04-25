828cf5957b0fb400d4194ed5003970d4fece9af0
bhaveesh09 <bhaveesh.09@gmail.com>
2021-04-24 18:27:49 -0700

    leapyear.py with error handling comment update

--- leapyear.py
+++ leapyear.py
@@ -1,10 +1,10 @@
 #Bhaveesh Beemireddy CS362 HW#1
 #run program and enter the year you want to calcuate for output
 x= int(input("enter year:")) #take in user inut for leap year calucation
-if (x<0):
+if (x<0):                #Now check if user input is a positive number
     print("Enter a postive number, program has quit")
-    quit                        #Now check if user input is a positive number
-elif (x % 4== 0) and (x % 100 != 0): #conditions for leap year
+                      
+elif (x % 4== 0) and (x % 100 != 0): #conditions for leap year updated
     print(x, "is a leap year")
 
 elif (x % 400 == 0):

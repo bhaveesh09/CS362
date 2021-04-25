1c57576e35924cb66ab1149e3399494159f58efe
bhaveesh09 <bhaveesh.09@gmail.com>
2021-04-24 18:19:33 -0700

    working leapyear program

--- leapyear.py
+++ leapyear.py
@@ -1,10 +1,9 @@
 #Bhaveesh Beemireddy CS362 HW#1
 #run program and enter the year you want to calcuate for output
 x= int(input("enter year:")) #take in user inut for leap year calucation
-
 if (x<0):
     print("Enter a postive number, program has quit")
-    quit
+    quit                        #Now check if user input is a positive number
 elif (x % 4== 0) and (x % 100 != 0): #conditions for leap year
     print(x, "is a leap year")
 

ab4eadb30c7c8a1fbf79a0f244688af52d120ec9
bhaveesh09 <72036215+bhaveesh09@users.noreply.github.com>
2021-05-22 12:56:14 -0700

    leapyear.py updated

--- leapyear.py
+++ leapyear.py
@@ -1,14 +1,15 @@
-#Bhaveesh Beemireddy CS362 HW#1
-#run program and enter the year you want to calcuate for output
-x= int(input("enter year:")) #take in user inut for leap year calucation
-if (x<0):                #Now check if user input is a positive number
-    print("Enter a postive number, program has quit")
-                      
-elif (x % 4== 0) and (x % 100 != 0): #conditions for leap year updated
-    print(x, "is a leap year")
-
-elif (x % 400 == 0):
-    print(x, "is a leap year")
-
-else:
-    print(x, "is not a leap year")
+def leapYear(x):
+   
+    if (x<0):                #Now check if user input is a positive number
+        print("Enter a postive number, program has quit")
+        return False           
+    elif (x % 4== 0) and (x % 100 != 0): #conditions for leap year updated
+        print(x, "is a leap year")
+        return True
+    elif (x % 400 == 0):
+        print(x, "is a leap year")
+        return True
+    else:
+        print(x, "is not a leap year")
+        return False
+    
